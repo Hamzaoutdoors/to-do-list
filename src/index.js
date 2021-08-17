873c8a0644597339a1508c4b0e998c772bd1f08f
Hamza <hamzaoutdoors@gmail.com>
2021-08-17 23:56:05 +0100

    fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import _ from 'lodash';
 import './style.css';
 
-
 function component() {
   const element = document.createElement('div');
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
     ],
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [{
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            }
-        ],
-    },
+  },
 };
