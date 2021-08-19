81beb0d0dae7ca0a7541a3f7d390188d19172de9
Hamza <hamzaoutdoors@gmail.com>
2021-08-19 22:50:33 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 import './style.css';
 import IsCompleted from './modules/Status.js';
+// import ChangeToDoList from './modules/ChangeList.js';
+
+const input = document.getElementById('input');
 
 const ul = document.querySelector('ul');
-const tasks = JSON.parse(localStorage.getItem('ToDo')) || [{
-  description: 'Complete To Do list project',
-  completed: false,
-  index: 0,
-},
-{
-  description: 'Learn Object-oriented Programming',
-  completed: true,
-  index: 1,
-},
-{
-  description: 'Help Mom',
-  completed: false,
-  index: 2,
-},
-{
-  description: 'Meet friends',
-  completed: false,
-  index: 3,
-},
-];
+let tasks = JSON.parse(localStorage.getItem('ToDo')) || [];
 
 const displayItems = () => {
   tasks.forEach((item, index) => {
     const CHECK = item.completed ? 'checked' : '';
     const TROUGHLINE = item.completed ? 'line-through' : '';
     item.index = index;
-    ul.innerHTML += `<li><input type="checkbox" class="checkbox" id="checkbox-${item.index}" ${CHECK}><h4 class="text ${TROUGHLINE}">${item.description}</h4><img class="drop" src="https://img.icons8.com/ios-glyphs/30/000000/menu-2.png" alt="vertical menu"/></li>`;
+    ul.innerHTML += `<li><input type="checkbox" class="checkbox" id="checkbox-${item.index}" ${CHECK}><input class="text ${TROUGHLINE}" type="text" value ="${item.description}"><img class="drop" src="https://img.icons8.com/ios-glyphs/30/000000/menu-2.png" alt="vertical menu"/></li>`;
   });
   IsCompleted.completeToDo(tasks);
 };
 displayItems();
+
+function addToDO(toDo, id) {
+  const list = JSON.parse(localStorage.getItem('ToDo')) || [];
+  tasks = [...list, { description: toDo, completed: false, index: id }];
+}
+
+input.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    const task = input.value;
+    if (task) {
+      addToDO(task, tasks.length);
+      displayItems();
+      IsCompleted.updateLocalStorage(tasks);
+    }
+    input.value = '';
+  }
+});
