8d1a42db37c956feade6e74454e67ef5bf745f2d
Hamza <hamzaoutdoors@gmail.com>
2021-08-26 00:06:33 +0100

    export functions

--- src/add.remove.test.js
+++ src/add.remove.test.js
@@ -1,21 +1,12 @@
-import removeItem from "./removeItem";
+import removeItem from './removeItem.js';
 
-
-describe('Remove item from list', ()=> {
-    test('Add one new item to the list', () => {
-        document.body.innerHTML =
-        '<div>' +
-        '  <ul id="list"></li>' +
-        '</div>';
-        removeItem();
-        const list = document.querySelectorAll('#list li');
-        expect(list).toHaveLength(list.length - 1);
-    }); 
-})
-
-
-
-
-
-
-
+describe('Remove item from list', () => {
+  test('remove one item from the list', () => {
+    document.body.innerHTML = '<div>'
+        + '  <ul id="list"></li>'
+        + '</div>';
+    removeItem();
+    const list = document.querySelectorAll('#list li');
+    expect(list).toHaveLength(list.length - 1);
+  });
+});
--- src/removeItem.js
+++ src/removeItem.js
@@ -1,23 +1,25 @@
-export default remove = () => {
-    window.addEventListener('click', (e) => {
-      const ul = document.getElementById('list');
-      if (e.target && e.target.className.includes('trash')) {
-        const id = parseInt(e.target.parentNode.id, 10);
-        tasks = tasks.filter((task) => task.index !== id);
-        ul.innerHTML = '';
-        displayItems();
-        IsCompleted.updateLocalStorage(tasks);
-      } else if (e.target && !e.target.className.includes('text')) {
-        const allLi = document.querySelector('#list').childNodes;
-        allLi.forEach((list) => {
-          const innerInput = list.querySelector('.text');
-  
-          innerInput.parentNode.querySelector('.trash').className = 'fa fa-trash-o trash d-none';
-          innerInput.parentNode.querySelector('.open').classList.remove('d-none');
-          innerInput.parentNode.style.backgroundColor = '';
-          innerInput.style.backgroundColor = '';
-        });
-      }
-    });
-  };
+import displayItems from './index.js';
 
+let tasks = JSON.parse(localStorage.getItem('ToDo')) || [];
+
+export default function remove() {
+  window.addEventListener('click', (e) => {
+    const ul = document.getElementById('list');
+    if (e.target && e.target.className.includes('trash')) {
+      const id = parseInt(e.target.parentNode.id, 10);
+      tasks = tasks.filter((task) => task.index !== id);
+      ul.innerHTML = '';
+      displayItems();
+      localStorage.setItem('ToDo', JSON.stringify(tasks));
+    } else if (e.target && !e.target.className.includes('text')) {
+      const allLi = document.querySelector('#list').childNodes;
+      allLi.forEach((list) => {
+        const innerInput = list.querySelector('.text');
+        innerInput.parentNode.querySelector('.trash').className = 'fa fa-trash-o trash d-none';
+        innerInput.parentNode.querySelector('.open').classList.remove('d-none');
+        innerInput.parentNode.style.backgroundColor = '';
+        innerInput.style.backgroundColor = '';
+      });
+    }
+  });
+}
